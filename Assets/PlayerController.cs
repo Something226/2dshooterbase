c5d7444b59f08e5c5ea400d2f3e6618829ae23aa
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2019-10-14 09:40:02 +0200

    Enkel förflyttning av spelarens skepp

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveX, moveY) * Time.deltaTime * 2;
+
+        transform.Translate(movement);
+
+	}
+}
